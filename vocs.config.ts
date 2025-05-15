fb0b37386bd0b8402d020e1e2af4e28224ed8a1f
Stephen Fraser <stephen.fraser@digirati.com>
2025-05-15 20:56:48 +0100

    Initial commit

---
+++ vocs.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  title: 'IIIF Exhibition Viewer',
+  sidebar: [
+    {
+      text: 'About',
+      link: '/about',
+    },
+  ],
+})
